f9b120712348f62b14433ae5f453b7bb3b9609bb
Sean Murphy <sean@iamseanmurphy.com>
2026-02-10 12:34:20 +0000

    2.14.8

--- modula-best-grid-gallery/assets/js/admin/general-settings/index.asset.php
+++ modula-best-grid-gallery/assets/js/admin/general-settings/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ff0ad0fb636483e122b3');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '65e83fdd0c7a463cf3ca');
